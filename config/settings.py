67f6b505abac5961b60ba1d0a096c7ae85e9571d
ConvRes <nggreco@illinois.edu>
2026-02-13 14:52:31 -0600

    Update settings.py
    Added configurations

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,12 @@
+MODEL_NAME = "allenai/specter2_base" #opensource embedding model
 
+MAX_TOKENS = 512 #fundamental unit of text is 1 token (~4 characters)
+CHUNK_SIZE = 400 
+CHUNK_OVERLAP = 50 #Overlap token range to help with context flow
+
+TOP_K_MESH = 15 #??
+MIN_SIMILARITY = 0.35 # Minimum cosine similarity for assignment
+
+EXCLUDED_MESH_TERMS = {
+  "Humans", "Animals", "Male", "Female"
+}
