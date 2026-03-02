aa8082e2b2feabc86f62fd7bd567108fe2eb8cf1
CaridadDelCobre <nicholas.g.greco@gmail.com>
2026-03-01 20:47:48 -0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -4,8 +4,8 @@ MAX_TOKENS = 512 #fundamental unit of text is 1 token (~4 characters)
 CHUNK_SIZE = 400 
 CHUNK_OVERLAP = 50 #Overlap token range to help with context flow
 
-TOP_K_MESH = 15 # Number of MeSH terms assigned
-MIN_SIMILARITY = 0.85 # Minimum cosine similarity for assignment
+TOP_K_MESH = 30 # Number of MeSH terms assigned
+MIN_SIMILARITY = 0.70 # Minimum cosine similarity for assignment
 
 EXCLUDED_MESH_TERMS = {
   "Humans", "Animals", "Male", "Female"
